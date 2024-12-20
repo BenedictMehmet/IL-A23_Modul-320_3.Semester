766d69cf8851225426bab90683de25d03abd3958
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2024-12-20 10:10:08 +0100

    add interface and abstract methods

---
+++ KW51/Übungen/Übungen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A23_Modul_320_3.Semester.KW51.Übungen
+{
+    internal class Übungen
+    {
+        /*
+         * 1. Erstelle eine abstrakte Klasse und erkläre mir, wie du die abstrakte Klasse verwendenw willst
+         * 
+         * 
+         * 2. Erstelle ein interface. Erkläre die Möglichkeiten von einem Interface
+         * 
+         * 
+         * 
+         */
+    }
+}
