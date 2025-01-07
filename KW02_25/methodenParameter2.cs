9b077b46c91af2d5af53bfb51f3cec46e5fa8a78
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-01-07 21:59:19 +0100

    add week02_25

---
+++ KW02_25/methodenParameter2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A23_Modul_320_3.Semester.KW02_25
+{
+    internal class methodenParameter2
+    {
+        internal double Tanken(double Menge, int gefahreneKilometer)
+        {
+            double literPro100km = Menge * 100 / gefahreneKilometer;
+
+            return literPro100km;
+        }
+    }
+
+    class MethodenParameter2
+    {
+
+        static void Main(string[] args)
+        {
+            methodenParameter2 PKW = new methodenParameter2();
+            double verbrauch;
+            verbrauch = PKW.Tanken(43, 689);
+
+            Console.WriteLine("Verbrauch: {0:f} Liter pro 100 km", verbrauch);
+        }
+    }
+}
+
+
